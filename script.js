9c2854f29ce46c25749a17304f2d7b209f6a7b72
TaniaPrete <166862270+TaniaPrete@users.noreply.github.com>
2024-05-30 20:30:10 +0200

    primo commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Traccia 2:
+
+// Crea una rubrica contenente una lista di contatti e con le seguenti funzionalita':
+
+// Mostrare o nascondere la lista dei contatti
+// Popolare la tabella con i contatti presenti nell’array di partenza
+// Aggiungere un nuovo contatto
+// Eliminare un contatto in rubrica
+// EXTRA:
+
+// Modificare un contatto presente in rubrica
+// Non permettere l'inserimento di due contatti con lo stesso numero telefonico
+// Richiedere conferma prima di eliminare un contatto
+
+console.log('ciao mondo');
